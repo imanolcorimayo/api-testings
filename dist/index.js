5e5654249270dbbfb7fc7fe3b65641f6e8e3f9e0
Imanol <imanol@meowtel.com>
2025-06-28 21:23:08 -0300

    new challenge

--- dist/index.js
+++ dist/index.js
@@ -6,21 +6,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
+app.use(express_1.default.json());
+// Register every request
+app.use((req, res, next) => {
+    console.log(`New request: ${req.method} ${req.url}`);
+    next();
+});
 app.get('/status', (req, res) => {
     res.send({ damaged_system: "navigation" });
 });
 app.get('/repair-bay', (req, res) => {
     // Send html
     res.setHeader('Content-Type', 'text/html');
-    res.write('<!DOCTYPE html>\n');
-    res.write('<html>\n');
-    res.write('<head>\n');
-    res.write('    <title>Repair</title>\n');
-    res.write('</head>\n');
-    res.write('<body>\n');
-    res.write('    <div class="anchor-point">NAV-01</div>\n');
-    res.write('</body>\n');
-    res.write('</html>\n');
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>Repair</title>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<div class="anchor-point">NAV-01</div>');
+    res.write('</body>');
+    res.write('</html>');
     res.send();
 });
 //Tercera Llamada: POST /teapot
@@ -28,6 +34,27 @@ app.get('/repair-bay', (req, res) => {
 app.post('/teapot', (req, res) => {
     res.status(418).send("I'm a teapot");
 });
+app.post('/phase-change-diagram', (req, res) => {
+    // Check if query are provided
+    if (!req.query.pressure) {
+        return res.status(400).send('Pressure query parameter is required');
+    }
+    // Get pressure from request query parameters
+    const pressure = parseFloat(req.query.pressure);
+    if (isNaN(pressure)) {
+        return res.status(400).send('Invalid pressure value');
+    }
+    const LIQUID_CONSTANT = -4.21428;
+    const LIQUID_SLOPE = 4061.22448;
+    const LIQUID_VOLUME = (pressure - LIQUID_CONSTANT) / LIQUID_SLOPE;
+    const VAPOR_CONSTANT = -4.21428;
+    const VAPOR_SLOPE = 4061.22448;
+    const VAPOR_VOLUME = (pressure - VAPOR_CONSTANT) / VAPOR_SLOPE;
+    res.send({
+        specific_volume_liquid: LIQUID_VOLUME,
+        specific_volume_vapor: VAPOR_VOLUME
+    });
+});
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,16 +6,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Register every request
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next) => {
   console.log(`New request: ${req.method} ${req.url}`);
   next();
 });
 
-app.get('/status', (req, res) => {
+app.get('/status', (req: Request, res: Response) => {
   res.send({ damaged_system: "navigation" });
 });
 
-app.get('/repair-bay', (req, res) => {
+app.get('/repair-bay', (req: Request, res: Response) => {
 
   // Send html
   res.setHeader('Content-Type', 'text/html');
@@ -31,10 +31,39 @@ app.get('/repair-bay', (req, res) => {
   res.send();
 });
 
-//Tercera Llamada: POST /teapot
-// Retorna un código de estado HTTP 418 (I'm a teapot).
+app.post('/phase-change-diagram', (req: Request, res: Response) => {
 
-app.post('/teapot', (req, res) => {
+  const pressure = req.query.pressure && typeof req.query.pressure === 'string' ? parseFloat(req.query.pressure) : null;
+
+  if (pressure === null || isNaN(pressure)) {
+    res.status(400).send({ error: 'Invalid or missing pressure parameter' });
+    return;
+  }
+  
+  // Get pressure from request query parameters
+  const LIQUID_CONSTANT = -4.21428;
+  const LIQUID_SLOPE = 4061.22448;
+
+  const LIQUID_VOLUME =  (pressure - LIQUID_CONSTANT) / LIQUID_SLOPE;
+
+  // Round 4 decimal places
+  const roundedLiquidVolume = Math.round(LIQUID_VOLUME * 10000) / 10000;
+
+  const VAPOR_CONSTANT = 10.00116;
+  const VAPOR_SLOPE = -0.33170;
+
+  const VAPOR_VOLUME = (pressure - VAPOR_CONSTANT) / VAPOR_SLOPE;
+
+  // Round 4 decimal places
+  const roundedVaporVolume = Math.round(VAPOR_VOLUME * 10000) / 10000;
+
+  res.send({
+    specific_volume_liquid: roundedLiquidVolume,
+    specific_volume_vapor: roundedVaporVolume
+  });
+});
+
+app.post('/teapot', (req: Request, res: Response) => {
 
   res.status(418).send("I'm a teapot");
 });
